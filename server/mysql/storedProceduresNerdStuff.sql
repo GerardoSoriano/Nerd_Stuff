faf344aebcb52de3c4e68a57ef3d3e2e243c7ef1
BAD-WOLF\Manuel <mainloader@gmail.com>
2017-04-11 17:26:22 -0500

    SP de tabla Producto

--- server/mysql/storedProceduresNerdStuff.sql
+++ server/mysql/storedProceduresNerdStuff.sql
@@ -4,11 +4,11 @@
 delimiter $$
 create procedure agregarUsuario(in nombreU nvarchar(50), in primerNom nvarchar (50), in segundoNom nvarchar(50), in apellidoPat nvarchar (50),
 	in apellidoMat nvarchar (50), in correo nvarchar(50), in contra nvarchar(20), in fechaNac date, in sexo enum('Masculino', 'Femenino'),
-    in metodoPago enum('DepositoBancario', 'Tarjeta'),	in fechaReg date, idPat smallint unsigned)
+    in metodoPago enum('DepositoBancario', 'Tarjeta'), idPat smallint unsigned)
     begin 
 		insert into usuario(nombreUsuario, primerNombre, segundoNombre, apellidoPaterno, apellidoMaterno, email, contrasena, fechaNacimiento,
 		genero, formaPago, fechaRegistro, idPatrocinador) 
-		values (nombreU, primerNom, segundoNom, apellidoPat, apellidoMat, correo, contra, fechaNac, sexo, metodoPago, fechaReg, idPat);
+		values (nombreU, primerNom, segundoNom, apellidoPat, apellidoMat, correo, contra, fechaNac, sexo, metodoPago, now(), idPat);
 	end
 $$
 
@@ -56,7 +56,7 @@ $$
 
 /*SP para traer mi lista de invitados*/
 delimiter $$
-create procedure misInvitados(in idU nvarchar(50))
+create procedure misInvitados(in idU smallint unsigned)
 	begin
 		select idUsuario, nombreUsuario, concat(primerNombre, ' ', segundoNombre, ' ', apellidoPaterno, ' ', apellidoMaterno) as nombre, idPatrocinador
         from usuario
@@ -64,14 +64,84 @@ create procedure misInvitados(in idU nvarchar(50))
 	end
 $$
 
+/*SP para cambiar forma de pago*/
+delimiter $$
+create procedure cambiarFormaPago(in nombreU nvarchar(50), in metodoPago enum('DepositoBancario', 'Tarjeta'))
+	begin
+		update usuario
+        set formaPago = metodoPago
+        where nombreUsuario = nombreU;
+	end
+$$
 
 
+/*TABLA DOMICILIO*/
+
+/*SP para agregar un domicilio*/
+delimiter $$
+create procedure agregarDomicilio(in tituloDom nvarchar(50), in nombreCalle nvarchar(255), in numeroDom nvarchar(10),
+	in nombreColonia nvarchar(255), in nombreCiudad nvarchar(255), in nombrePais nvarchar(50), in idU smallint unsigned)
+	begin
+		insert into domicilioEntrega(tituloDomicilio, calle, numeroDomicilio, colonia, ciudad, pais, idUsuario)
+        values (tituloDom , nombreCalle, numeroDom, nombreColonia, nombreCiudad, nombrePais, idU);
+    end
+$$
+
+/*SP para traer la lista de domicilios de un usuario*/
+delimiter $$
+create procedure mostrarDomicilios(in idU smallint unsigned)
+	begin
+		select idDomicilio, tituloDomicilio
+        from domicilioEntrega
+        where idUsuario = idU;
+	end
+$$
+
+/*SP para traer la información del domicilio seleccionado*/
+delimiter $$
+create procedure obtenerDomicilio(in idD smallint unsigned, in idU smallint unsigned)
+	begin
+		select tituloDomicilio, concat(calle, ' ', numeroDomicilio, ', ', colonia, ', ', ciudad, ', ', pais) as domicilio
+        from domicilioEntrega
+        where idDomicilio = idD and idUsuario = idU;
+	end
+$$
+
+/*SP para modificar un domicilio*/
+delimiter $$
+create procedure modificarDomicilio(in idD smallint unsigned, in tituloDom nvarchar(50), in nombreCalle nvarchar(255), in numeroDom nvarchar(10),
+	in nombreColonia nvarchar(255), in nombreCiudad nvarchar(255), in nombrePais nvarchar(50), in idU smallint unsigned)
+    begin
+		update domicilioEntrega
+		set tituloDomicilio = tituloDom,
+			calle = nombreCalle,
+			numeroDomicilio = numeroDom,
+			colonia = nombreColonia,
+			ciudad = nombreCiudad,
+			pais = nombrePais
+		where idDomicilio = idD and idUsuario = idU;
+    end
+$$
 
 
+/*TABLA PRODUCTO*/
 
+/*SP para traer los productos de una categoría*/
+delimiter $$
+create procedure productosCategoria(in cat nvarchar(50))
+	begin
+		select idProducto, nombreProducto, costo, puntaje, descripcion, direccionFoto
+        from producto
+        where categoria = cat;
+	end
+$$
 
-
-
-
-
-
+/*SP para traer la información de un producto en específico*/
+delimiter $$
+create procedure mostrarProducto(in idP smallint unsigned)
+	begin
+		select nombreProducto, costo, puntaje, descripcion, direccionFoto
+        from producto
+        where idProducto = idP;
+    end
+$$
