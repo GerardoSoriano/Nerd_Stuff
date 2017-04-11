7c2b118ea7f825a343fa4b4df8d45b4a4e051d6d
BAD-WOLF\Manuel <mainloader@gmail.com>
2017-04-11 15:21:58 -0500

    Stored Procedures para tabla Usuario

--- server/mysql/storedProceduresNerdStuff.sql
+++ server/mysql/storedProceduresNerdStuff.sql
@@ -3,7 +3,7 @@
 /*SP para agregar Usuarios*/
 delimiter $$
 create procedure agregarUsuario(in nombreU nvarchar(50), in primerNom nvarchar (50), in segundoNom nvarchar(50), in apellidoPat nvarchar (50),
-	in apellidoMat nvarchar (50), in correo nvarchar(50), in contra nvarchar(255), in fechaNac date, in sexo enum('Masculino', 'Femenino'),
+	in apellidoMat nvarchar (50), in correo nvarchar(50), in contra nvarchar(20), in fechaNac date, in sexo enum('Masculino', 'Femenino'),
     in metodoPago enum('DepositoBancario', 'Tarjeta'),	in fechaReg date, idPat smallint unsigned)
     begin 
 		insert into usuario(nombreUsuario, primerNombre, segundoNombre, apellidoPaterno, apellidoMaterno, email, contrasena, fechaNacimiento,
@@ -16,17 +16,17 @@ $$
 delimiter $$
 create procedure datosPersonales(in nombreU nvarchar(50))
 	begin
-		select nombreUsuario, primerNombre, segundoNombre, apellidoPaterno, apellidoMaterno, email, contrasena, fechaNacimiento,
+		select idUsuario, nombreUsuario, primerNombre, segundoNombre, apellidoPaterno, apellidoMaterno, email, contrasena, fechaNacimiento,
         genero, formaPago, fechaRegistro, idPatrocinador
         from usuario
         where nombU = nombreUsuario;
 	end
 $$
 
-/*SP para modificar los datos del Usuario*/
+/*SP para modificar los datos del Usuario, contraseña y nombre de usuario no se modifican*/
 delimiter $$
 create procedure modificarDatosPersonales(in nombreU nvarchar(50), in primerNom nvarchar (50), in segundoNom nvarchar(50),
-	in apellidoPat nvarchar (50), in apellidoMat nvarchar (50), in correo nvarchar(50), in contra nvarchar(255), in fechaNac date,
+	in apellidoPat nvarchar (50), in apellidoMat nvarchar (50), in correo nvarchar(50), in contra nvarchar(20), in fechaNac date,
     in sexo enum('Masculino', 'Femenino'), in metodoPago enum('DepositoBancario', 'Tarjeta'), in fechaReg date, idPat smallint unsigned)
     begin
 		update usuario
@@ -35,12 +35,43 @@ create procedure modificarDatosPersonales(in nombreU nvarchar(50), in primerNom 
             apellidoPaterno = apellidoPat,
             apellidoMaterno = apellidoMat,
             email = correo,
-            contrasena = contra,
             fechaNacimiento = fechaNac,
             genero = sexo,
             formaPago = metodoPago,
             fechaRegistro = fechaReg,
             idPatrocinador = idPat
-		where nombreUsuario = nombreU;
+		where nombreUsuario = nombreU and contrasena = contra;
     end
 $$
+
+/*SP para modificar contraseña*/
+delimiter $$
+create procedure modificarContrasena(in nombreU nvarchar(50), in contraOriginal nvarchar(20), in contraNueva(20))
+	begin
+		update usuario
+        set contrasena = contraNueva
+        where nombreUsuario = nombreU and contrasena = contraOriginal;
+	end
+$$
+
+/*SP para traer mi lista de invitados*/
+delimiter $$
+create procedure misInvitados(in idU nvarchar(50))
+	begin
+		select idUsuario, nombreUsuario, concat(primerNombre, ' ', segundoNombre, ' ', apellidoPaterno, ' ', apellidoMaterno) as nombre, idPatrocinador
+        from usuario
+        where idPatrocinador = idU;
+	end
+$$
+
+
+
+
+
+
+
+
+
+
+
+
