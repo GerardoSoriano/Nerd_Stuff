ef386323f33975770f5846df3ecb26ff18c154d4
Gerardo Soriano <gerardosoriano97@gmail.com>
2017-05-04 15:01:41 -0500

    Ya esta el login y registro, pero separados

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', ['sass', 'sass_login'], function() {
 
     browserSync.init({
         server: "./app"
@@ -11,6 +11,8 @@ gulp.task('serve', ['sass'], function() {
 
     gulp.watch("app/office/scss/**/*.scss", ['sass']);
     gulp.watch("app/office/scss/main.scss", ['sass']);
+    gulp.watch("app/login/scss/**/*.scss", ['sass_login']);
+    gulp.watch("app/login/scss/main.scss", ['sass_login']);
     gulp.watch("app/*.html").on('change', browserSync.reload);
 });
 
@@ -19,6 +21,12 @@ gulp.task('sass', function() {
     return gulp.src("app/office/scss/main.scss")
         .pipe(sass())
         .pipe(gulp.dest("app/office/css"))
+        .pipe(browserSync.stream());
+});
+gulp.task('sass_login', function() {
+    return gulp.src("app/login/scss/main.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("app/login/css"))
         .pipe(browserSync.stream());
 });
 
