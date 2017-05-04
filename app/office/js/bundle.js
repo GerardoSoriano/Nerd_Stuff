facbfc7ff622398411fc9ffab550a7934c7506f0
Gerardo Soriano <gerardosoriano97@gmail.com>
2017-05-04 17:21:56 -0500

    Se arregla el carrusel de todas las páginas

--- app/office/js/bundle.js
+++ app/office/js/bundle.js
@@ -4,37 +4,6 @@ $(document).ready(function(){
     $('div[class^="header"]').toggleClass('header header-max');
     $('div[class^="content"]').toggleClass('content content-max');
   });
-  $('div.categoria>div.offers').slick({
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true
-  });
-  $('div.productContainer').slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplaySpeed: 2000,
-    responsive: [
-      {
-        breakpoint: 1572,
-        settings: {
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 1254,
-        settings: {
-          slidesToShow: 2
-        }
-      },
-      {
-        breakpoint: 936,
-        settings: {
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
   $('div.receipt>i[role="button"]').on("click",function(){
     $('body>div[class^="modal"]').toggleClass('modal-hide modal-show');
   });
