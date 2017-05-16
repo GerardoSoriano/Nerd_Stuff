7431c4d3520ef62274b4d19d1a6d876ed34c1280
Gerardo Soriano <gerardosoriano97@gmail.com>
2017-05-16 17:35:36 -0500

    Se comienza a hacer el correo, aun falta que lo mande con formato

---
+++ server/php/controller/sendEmailTEST.php
@@ -0,0 +1,24 @@
+<?php
+require '../vendor/autoload.php';
+
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->SMTPDebug = 2;
+$mail->Debugoutput = 'html';
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 587;
+$mail->SMTPSecure = 'tls';
+$mail->SMTPAuth = true;
+$mail->Username = "gerardosoriano97@gmail.com";
+$mail->Password = "jgsoriano";
+$mail->setFrom('gerardosoriano97@gmail.com', 'Gerardo Soriano');
+$mail->addAddress('gerardosoriano97@gmail.com', 'Juan Soto');
+$mail->Subject = 'Test para mandar correo con formato';
+$mail->msgHTML(file_get_contents('../../../app/emails/template.html'), dirname(__FILE__));
+$mail->AltBody = 'This is a plain-text message body';
+if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message sent!";
+}
+ ?>
