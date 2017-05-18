d412273a5f1fd0e714c506014ba707c4b4f5da8e
Gerardo Soriano <gerardosoriano97@gmail.com>
2017-05-18 02:00:03 -0500

    Se corrige el JSON de productosPorCategoria.php

--- server/php/controller/productosPorCategoria.php
+++ server/php/controller/productosPorCategoria.php
@@ -1,19 +1,21 @@
 <?php
 include_once(dirname(__DIR__).'/data/productoCRUD.php');
+include_once(dirname(__DIR__).'/data/categoriaCRUD.php');
 include_once(dirname(__DIR__).'/data/jwt_helper.php');
 
 $token = $_POST['token'];
 $token = JWT::decode($token,'9286');
 if (array_key_exists("idUsuario",$token)){
-  $idCategoria = $_POST['idCategoria'];
-
-  $productos = new Producto();
-  $productos->setIdCategoria($idCategoria);
-  $productos = ProductoMetodos::ProductosPorCategoria($productos);
-
   $return = array();
-  $return['msg'] = "success";
-  $return['productos'] = $productos;
-  print_r($return);
+  $categorias = CategoriaMetodos::ObtenerCategorias();
+  foreach ($categorias as $key => $value) {
+    $productos = new Producto();
+    $productos->setIdCategoria($value['idCategoria']);
+
+    $productos = ProductoMetodos::ProductosPorCategoria($productos);
+    $return[$key]['productos'] = $productos;
+    $return[$key]['nombreCategoria'] = $value['nombreCategoria'];
+  }
+  print_r(json_encode($return));
 }
  ?>
