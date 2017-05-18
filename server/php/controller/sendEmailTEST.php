a91912cfd141bc679dcb7c615c2d5864ceb00540
Gerardo Soriano <gerardosoriano97@gmail.com>
2017-05-18 00:58:04 -0500

    Ya esta el diseño del correo. Dependencia de PHP añadida, dar composer install en la raiz del proyecto

--- server/php/controller/sendEmailTEST.php
+++ server/php/controller/sendEmailTEST.php
@@ -15,6 +15,8 @@ $mail->setFrom('gerardosoriano97@gmail.com', 'Gerardo Soriano');
 $mail->addAddress('gerardosoriano97@gmail.com', 'Juan Soto');
 $mail->Subject = 'Test para mandar correo con formato';
 $mail->msgHTML(file_get_contents('../../../app/emails/template.html'), dirname(__FILE__));
+$mail->AddEmbeddedImage('../../../app/resources/img/imagotipoHorizontal_NerdStuff2_lq.png', 'companyLogo');
+$mail->IsHTML(true);
 $mail->AltBody = 'This is a plain-text message body';
 if (!$mail->send()) {
     echo "Mailer Error: " . $mail->ErrorInfo;
