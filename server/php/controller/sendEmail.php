1628dab5c7950a57320a50a043ec190707df7e38
José Everardo Hernández Díaz <everardo.hernandezdz@gmail.com>
2017-05-30 17:24:24 -0500

    Se arreglo el email

--- server/php/controller/sendEmail.php
+++ server/php/controller/sendEmail.php
@@ -1,39 +1,34 @@
 <?php
 include_once(dirname(__DIR__).'/data/usuarioCRUD.php');
 include_once(dirname(__DIR__).'/data/jwt_helper.php');
-require '../vendor/autoload.php';
 
 $data = json_decode($_POST['json']);
 $token = $data->token;
 $token = JWT::decode($token,'9286');
-//si el token es valido
-if (array_key_exists("idUsuario",$token)){
-  $message = file_get_contents('../../../app/emails/template.html');
-  $message = str_replace('%nombre%', $data->nombre, $message);
-  $message = str_replace('%nombreUsuario%', $token->nombreUsuario, $message);
 
-  $mail = new PHPMailer;
-  $mail->isSMTP();
-  $mail->SMTPDebug = 0;
-  $mail->Debugoutput = 'html';
-  $mail->Host = 'smtp.gmail.com';
-  $mail->Port = 587;
-  $mail->SMTPSecure = 'tls';
-  $mail->SMTPAuth = true;
-  $mail->Username = "nerdstuffmx@gmail.com";
-  $mail->Password = "SuperQualifiedLemons";
-  $mail->setFrom('nerdstuffmx@gmail.com', 'Nerd Stuff');
-  $mail->addAddress($data->email, $data->nombre);
-  $mail->Subject = 'Haz sido invitado a formar parte de nuestras filas!';
-  $mail->msgHTML($message, dirname(__FILE__));
-  $mail->AddEmbeddedImage('../../../app/resources/img/imagotipoHorizontal_NerdStuff2_lq.png', 'companyLogo');
-  $mail->IsHTML(true);
-  $mail->AltBody = 'Nerd Stuff S.A. de C.V.';
-  if (!$mail->send()) {
-    echo "Mailer Error: " . $mail->ErrorInfo;
-  } else {
-    echo "Message sent!";
-  }
+if (array_key_exists("idUsuario",$token)){    
+  $emailto = $data->email;
+  $toname = $data->nombre;
+  $emailfrom = 'no-reply@nerdstuff.com.mx';
+  $fromname = 'Nerd Stuff S.A. de C.V.';
+  $subject = 'Haz sido invitado a formar parte de nuestras filas!';
+
+  $messagebody = file_get_contents('../../../app/emails/template.html');
+  $messagebody = str_replace('%nombre%', $data->nombre, $messagebody);
+  $messagebody = str_replace('%nombreUsuario%', $token->nombreUsuario, $messagebody);
+  $headers = 
+              'Return-Path: ' . $emailfrom . "\r\n" . 
+              'From: ' . $fromname . ' <' . $emailfrom . '>' . "\r\n" . 
+              'X-Priority: 3' . "\r\n" . 
+              'X-Mailer: PHP ' . phpversion() .  "\r\n" . 
+              'Reply-To: ' . $fromname . ' <' . $emailfrom . '>' . "\r\n" .
+              'MIME-Version: 1.0' . "\r\n" . 
+              'Content-Transfer-Encoding: 8bit' . "\r\n" . 
+              'Content-Type: text/html; charset=UTF-8' . "\r\n";
+  $params = '-f ' . $emailfrom;
+  mail($emailto, $subject, $messagebody, $headers, $params);
+} else {
+  echo "El usuario no tiene un token valido";
 }
 
- ?>
+?>
